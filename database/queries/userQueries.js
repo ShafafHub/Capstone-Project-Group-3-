7ee78474ea3cd73355a50c822b34959e957032c6
zia-sahil <ah.ziasahil@gmail.com>
2026-04-15 22:50:47 -0700

    rmove node_modules and add gitignore

--- database/queries/userQueries.js
+++ database/queries/userQueries.js
@@ -1,30 +1,4 @@
-// const db = require("../connection");
 
-// // Create user
-// const createUser = (name, email, password, callback) => {
-//     const sql = `
-//     INSERT INTO users (name, email, password)
-//     VALUES (?, ?, ?)
-//   `;
-
-//     db.run(sql, [name, email, password], function (err) {
-//         callback(err, this?.lastID);
-//     });
-// };
-
-// // Find user by email
-// const findUserByEmail = (email, callback) => {
-//     const sql = `SELECT * FROM users WHERE email = ?`;
-
-//     db.get(sql, [email], (err, row) => {
-//         callback(err, row);
-//     });
-// };
-
-// module.exports = {
-//     createUser,
-//     findUserByEmail,
-// };////
 
 const db = require("../connection");
 
