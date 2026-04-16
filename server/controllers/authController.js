7cdb4863e19d66a50ab4eabd469862991806c5fc
Elyas-Baheen <145687903+Elyas-Bahim@users.noreply.github.com>
2026-04-16 18:05:34 +0430

    update email format checker

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,7 +15,7 @@ export const register = async (req, res) => {
             });
         }
 
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
         if (!emailRegex.test(email)) {
             return res.status(400).json({
