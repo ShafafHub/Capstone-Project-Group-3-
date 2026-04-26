7db09941815b37c25e49b4d5c1d15460084887d1
zia-sahil <ah.ziasahil@gmail.com>
2026-04-26 00:09:45 -0700

    update user file  & Remove others file

--- database/migrations/001_user.js
+++ database/migrations/001_user.js
@@ -1,21 +1,20 @@
-exports.up = function (knex) {
-    return knex.schema.createTable("users", function (table) {
-        // 1. id
-        table.increments("id").primary();
 
-        // 2. email
-        table.string("email").notNullable().unique();
+// 1. id
+table.increments("id").primary();
 
-        // 3. password
-        table.string("password").notNullable();
+// 2. email
+table.string("email").notNullable().unique();
 
-        // 4. role
-        table.string("role").defaultTo("user");
+// 3. password
+table.string("password").notNullable();
 
-        // 5. timestamps
-        table.timestamps(true, true);
-    });
-};
+// 4. role
+table.string("role").defaultTo("user");
+
+// 5. timestamps
+table.timestamps(true, true);
+
+
 
 exports.down = function (knex) {
     return knex.schema.dropTable("users");
