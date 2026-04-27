16b7b7ee4c5361f4eb0524f727de83be7c45e463
saber <saber.seerat66@gmail.com>
2026-04-27 21:12:08 +0430

    refactor addReview to use Knex insert query

--- server/src/modules/review/review.model.js
+++ server/src/modules/review/review.model.js
@@ -1,6 +1,13 @@
-export const addReview = (db, data) => {
-  return db.run(
-    `INSERT INTO reviews (product_id, content) VALUES (?, ?)`,
-    [data.product_id, data.content]
-  );
+// /server/src/modules/review/review.model.js
+
+import db from "../../config/db.js";
+
+export const addReview = async (data) => {
+  const result = await db("reviews").insert({
+    product_id: data.product_id,
+    rating: data.rating,
+    content: data.content,
+  });
+
+  return result;
 };
