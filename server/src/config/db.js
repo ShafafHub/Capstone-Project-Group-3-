033244b76eea8a68f023f84ae451a2814f7fba7c
Elyas-Baheen <145687903+Elyas-Bahim@users.noreply.github.com>
2026-04-28 21:12:26 +0430

    feat: restructure project and add missing core modules

--- server/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-// --- DB connection ---
-export const dbPromise = open({
-  filename: './db.sqlite',
-  driver: sqlite3.Database,
-});
