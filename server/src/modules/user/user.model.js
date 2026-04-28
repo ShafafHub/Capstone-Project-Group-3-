f2f6fb42be24cfc2761cdc73f9ac62f3634b3c94
zia-sahil <ah.ziasahil@gmail.com>
2026-04-28 00:43:27 -0700

    replace raw SQL with knex in getUserById

--- server/src/modules/user/user.model.js
+++ server/src/modules/user/user.model.js
@@ -1,2 +1,12 @@
-export const getUserById = (db, id) =>
-    db.get('SELECT id, email FROM users WHERE id = ?', [id]);
+const db = require('../../config/db');
+
+async function getUserById(id) {
+    return await db('users')
+        .select('id', 'email')
+        .where({ id })
+        .first();
+}
+
+module.exports = {
+    getUserById,
+};
