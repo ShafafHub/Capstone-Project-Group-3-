28cd6b2c481111a297d11983d5613a0cee24b053
saber <saber.seerat66@gmail.com>
2026-04-29 21:05:00 +0430

    adding Women.jsx file

---
+++ client/src/pages/Women.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../features/product/product.api";
+import { Link } from "react-router-dom";
+
+export default function Women() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts().then((res) => {
+      const filtered = res.data.filter(
+        (p) => p.category === "women"
+      );
+      setProducts(filtered);
+    });
+  }, []);
+
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl mb-6">Women COLLECTION</h1>
+
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+        {products.map((p) => (
+          <Link to={`/product/${p.id}`} key={p.id}>
+            <div>
+              <img
+                src={p.image}
+                className="w-full h-[300px] object-contain bg-gray-100"
+              />
+              <h3>{p.name}</h3>
+              <p className="font-semibold">{p.description}</p>
+              <p className="text-sm text-gray-500">
+                ${p.price}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
