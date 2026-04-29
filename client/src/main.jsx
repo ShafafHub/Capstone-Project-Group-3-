564c73ec443ca44176c812b56e9302d923261fb6
Elyas-Baheen <145687903+Elyas-Bahim@users.noreply.github.com>
2026-04-29 19:01:51 +0430

    feat(app): setup root with router and ProductProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ProductProvider } from './context/ProductContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/index.css';
@@ -7,7 +8,9 @@ import './styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProductProvider>
+        <App />
+      </ProductProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
