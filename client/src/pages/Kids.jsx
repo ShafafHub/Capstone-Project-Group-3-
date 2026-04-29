e43adddaf3411d69c181a45d569b7331409958ce
zia-sahil <ah.ziasahil@gmail.com>
2026-04-28 22:55:54 -0700

    Kids page: fetch and display kids products

---
+++ client/src/pages/Kids.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../features/product/product.api";
+import { Link } from "react-router-dom";
+
+export default function Kids() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchProducts().then((res) => {
+            const menProducts = res.data.filter(
+                (item) => item.category === "kids"
+            );
+            setProducts(menProducts);
+        });
+    }, []);
+
+    return (
+        <div className="p-10">
+            <h1 className="text-2xl mb-6">KIDS COLLECTION</h1>
+
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+                {products.map((p) => (
+                    <Link to={`/product/${p.id}`} key={p.id}>
+                        <div>
+                            <img
+                                src={p.image}
+                                className="w-full h-[300px] object-contain bg-gray-100"
+                                alt={p.name}
+                            />
+                            <h3>{p.name}</h3>
+                            <p className="font-semibold">{p.description}</p>
+                            <p className="text-sm text-gray-500">${p.price}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
